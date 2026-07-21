3f9c519f06995850e8777cfff411ad3d6d34ef77
chottomatt <matheusrocha0110@gmail.com>
2026-07-20 23:15:15 -0300

    Genres added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 async function searchGenre() {
     const answer = await fetch("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptags&api_key=8d5e391006041eb0d8c1bda2b7631161&format=json");
-    const dados = await answer.json();
-    console.log(dados);
+    const data = await answer.json();
+    console.log(data);
+
+    const genres = data.tags.tag.map(function(tag) { 
+        return tag.name
+    });
+    console.log(genres);
 }
 
-searchGenreGenre()
+searchGenre();
