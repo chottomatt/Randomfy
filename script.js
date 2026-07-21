a22e436d3074edec86b5ad73b1f8b7cd686fc283
chottomatt <matheusrocha0110@gmail.com>
2026-07-20 23:46:23 -0300

    update

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 async function searchGenre() {
     const answer = await fetch("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptags&api_key=8d5e391006041eb0d8c1bda2b7631161&format=json");
     const data = await answer.json();
-    console.log(data);
 
     const genres = data.tags.tag.map(function(tag) { 
         return tag.name
     });
-    console.log(genres);
+
+    const randomIndex = Math.floor(Math.random() * genres.length);
+    const randomGenre = genres[randomIndex];
+    console.log(randomGenre);
 }
 
 searchGenre();
